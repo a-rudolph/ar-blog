f26064accc4b144bc4bb662d136d2de9ac8f754f
a-rudolph <adam.rudolph@live.ca>
2022-08-21 21:50:35 -0400

    remove link for blog home page

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,7 +1,13 @@
-<h2 class="mt-0">
-  <a class="link-secondary font-bold prose-heading no-underline" href="/">
-    ar-blog
-  </a>
-</h2>
+<script>
+	import { page } from '$app/stores';
+</script>
+
+{#if $page.url.pathname !== '/blog'}
+	<h2 class="mt-0">
+		<a class="link-secondary font-bold prose-heading no-underline" href="/blog">
+			ar-blog
+		</a>
+	</h2>
+{/if}
 
 <slot />
